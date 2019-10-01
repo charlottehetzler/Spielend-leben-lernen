ca91fb5c55249253279fee59e7989a087104a11a
Charlotte Hetzler <charlotte@hetzler.de>
2019-10-02 00:40:11 +0200

    add design on landing page incl. banner and pictures

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,14 @@
 import "bootstrap";
+
+jQuery(document).ready(function($) {
+  $(window).scroll(function() {
+    var scrollPos = $(window).scrollTop(),
+        navbar = $('.navbar');
+
+    if (scrollPos > 500) {
+      navbar.addClass('visible');
+    } else {
+      navbar.removeClass('visible');
+    }
+  });
+});
