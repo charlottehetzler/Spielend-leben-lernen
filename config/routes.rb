d8de9d4f8e0a031ea3ce81dc2b46127dc9ef04d2
Charlotte Hetzler <charlotte@hetzler.de>
2019-10-01 20:59:08 +0200

    add pages, routes and content & first style

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'legal/index', as: 'legal'
+  get 'contact/index', as: 'contact'
+  get 'about/index', as: 'about'
+  get 'offer/index', as: 'offer'
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
