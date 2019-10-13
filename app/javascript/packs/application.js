d10b99740f32e988e59b8b7b41fd8a5d53b33f55
Charlotte Hetzler <charlotte@hetzler.de>
2019-10-13 16:20:05 +0200

    add markers, update footer

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,7 @@
 import "bootstrap";
 
+// Toggle offer
+
 $(document).ready(function(){
   $("#one-more").hide();
   $("#one").click(function(){
@@ -35,19 +37,35 @@ $(document).ready(function(){
   });
 });
 
+// Mapbox
 
 import mapboxgl from 'mapbox-gl';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiY2hldHpsZXIiLCJhIjoiY2sxbmprZzVsMDhnMDNucnljYnZuYjg3MSJ9.bfpBECHO7lItHCBmwTbrIQ';
-console.log('hello');
 const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v9',
-  // center: [50.11, 8.68],
+  center: [50.054481, 8.643653],
   zoom: 5
 });
     new mapboxgl.Marker()
-    .setLngLat([8.67, 50.13], [8.65,49.76])
+    .setLngLat([8.67, 50.13])
+    .addTo(map);
+    new mapboxgl.Marker()
+    .setLngLat([8.65,49.76])
     .addTo(map);
 
 
+// active Class on Navbar
+
+function setActive() {
+  console.log("hello");
+  aObj = document.getElementByClassName('navbar-item');
+  for(i=0;i<aObj.length;i++) {
+    if(document.location.href.indexOf(aObj[i].href)>=0) {
+      aObj[i].className='active';
+    }
+  }
+}
+
+window.onload = setActive();
