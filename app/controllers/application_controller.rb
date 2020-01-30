e29a5c1f766ac50d0963c57da64dd2c03fd06d2e
Charlotte Hetzler <charlotte@hetzler.de>
2020-01-30 22:48:44 +0100

    delete code for basic http auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,3 @@
 class ApplicationController < ActionController::Base
-  # include HttpAuthConcern
-  # http_basic_authenticate_with name: ENV['USER_NAME'], password: ENV['PASSWORD']
   protect_from_forgery with: :exception
 end
