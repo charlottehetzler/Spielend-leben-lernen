b8c1ca765c28ece935ae83982e18077eac9d5ffc
Charlotte Hetzler <charlotte@hetzler.de>
2020-01-30 21:18:39 +0100

    add / route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'contact/index', as: 'contact'
   get 'about/index', as: 'about'
   get 'offer/index', as: 'offer'
+  get '/' to: 'pages#home'
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
